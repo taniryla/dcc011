ada0764ee72e47da1d3516e53287738e7ae74d00
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 09:55:35 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 11-isPalindrome
+Difficulty: Intermediate
+Prompt:
+- Write a function called isPalindrome that accepts a single string argument, then returns true or false depending upon whether or not the string is a palindrome.
+- A palindrome is a word or phrase that are the same forward or backward.
+- Casing and spaces are not included when considering whether or not a string is a palindrome.
+- If the length of the string is 0 or 1, return true.
+Examples:
+isPalindrome('SEI Rocks'); //=> false
+isPalindrome('rotor'); //=> true
+isPalindrome('A nut for a jar of tuna'); //=> true
+isPalindrome(''); //=> true
+-----------------------------------------------------------------*/
+// Your solution for 11-isPalindrome here:
+
+</div>
+`;
